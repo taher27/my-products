f50f29d01801bc4936d670eb2b9a007f0e8e2f1e
taher27 <34998783+taher27@users.noreply.github.com>
2025-06-30 09:53:30 +0000

    Unit test generated by RoostGPT
    Using AI Model gpt-4o

---
+++ src/test/java/com/bootexample4/products/model/Categories.java
@@ -0,0 +1,37 @@
+package com.bootexample4.products.model;
+
+public interface Categories {
+
+	interface valid {
+
+	}
+
+	interface invalid {
+
+	}
+
+	interface boundary {
+
+	}
+
+	interface integration {
+
+	}
+
+	interface getId {
+
+	}
+
+	interface getName {
+
+	}
+
+	interface getDescription {
+
+	}
+
+	interface getPrice {
+
+	}
+
+}
